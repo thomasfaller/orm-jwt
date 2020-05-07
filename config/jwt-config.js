8068a1f3d836038aa93f3303cb2a1ca1f7799f4c
Thomas Faller <thomasfaller@gmail.com>
2020-05-07 15:52:16 +0100

    gathered config into .env files

--- config/jwt-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  secret: "mysupersecret",
-  expiresIn: 600000, // expires in 10 minutes
-  notBefore: 60000, // we can use the token only after 1 minute
-  audience: "site-users",
-};
